9df20669bffd338aa3de4a7b7c10b73d4c858fae
Ali Tariq <alitariq4589@gmail.com>
2025-11-26 13:03:04 +0500

    Simplified entrypoint and removed additional commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
-if [[ ! -f ".runner" ]]; then
   if [[ -z "$GITHUB_REPO" || -z "$RUNNER_TOKEN" ]]; then
     echo "Missing GITHUB_REPO or RUNNER_TOKEN"
     exit 1
   fi
-  ./config.sh \
-    --url "${GITHUB_REPO}" \
-    --token "${RUNNER_TOKEN}" \
-    --name "$(hostname)" \
-    --unattended \
-    --replace
-  fi
-  ./run.sh
+
+./config.sh \
+  --url "${GITHUB_REPO}" \
+  --token "${RUNNER_TOKEN}"
+
+./run.sh
