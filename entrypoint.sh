74244335e83cc1995b7a28376f6a589b0818fd21
Ali Tariq <alitariq4589@gmail.com>
2025-11-26 16:26:39 +0500

    Added runner name for the github actions
    Previous build was successful and runners didnt terminate automatically this time. This commit will include the name of the runner in the github runners list. This way it will be easier for the users to identify the name of the board which is running the github actions package

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Set runner name
+RUNNER_NAME="${RUNNER_NAME:-$(hostname)}"
+
+# Execute only if .runner file does not exist
 if [[ ! -f ".runner" ]]; then
   if [[ -z "$GITHUB_REPO" || -z "$RUNNER_TOKEN" ]]; then
     echo "Missing GITHUB_REPO or RUNNER_TOKEN"
@@ -11,10 +15,10 @@ if [[ ! -f ".runner" ]]; then
   ./config.sh \
     --url "${GITHUB_REPO}" \
     --token "${RUNNER_TOKEN}" \
+    --name "${RUNNER_NAME}" \
     --unattended \
     --replace
 fi
 
+# Start the runner
 ./run.sh
-
-# sudo apt update
